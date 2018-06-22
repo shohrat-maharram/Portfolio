e48860fd2a8bb59d439164258a19f7bdae1640b5
Shohrat M. Maharramov <shohrat.maharramov@bakubus.local>
2018-06-22 17:45:36 +0400

    Working on Responsive Navbar

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
+
+    //Functions Executed on window resize
+    
+    $(window).resize(function () {
+        //Hide responsive nav
+        $("#navbar .responsive-nav").removeClass("active-nav");
+        $("#navbar").css("padding-bottom", "30px");
+        
+    });
+
+
     // Navbar section
     //Adding active class to clicked tab
     $("#navbar .container ul li a").click(function (e) {
         e.preventDefault();
         $("#navbar .container ul li .active").removeClass("active");
         $(this).addClass("active");
+    })
+
+    //Display&Hide responsive nav
+    $("#navbar .right-nav .menuContainer").click(function () {
+        $("#navbar .responsive-nav").toggleClass("active-nav");
+        $("#navbar").css("padding-bottom","10px");
     })
 });
