40cba3fb38cf029b4e00059ceaf126bf80c37f87
Shohrat M. Maharramov <shohrat.maharramov@bakubus.local>
2018-07-02 17:59:46 +0400

    Navbar complated

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -1,12 +1,27 @@
 $(document).ready(function () {
 
     //Functions Executed on window resize
-    
+
     $(window).resize(function () {
+
+        //Window width and height
+        var width = $(window).width();
+        var height = $(window).height();
         //Hide responsive nav
-        $("#navbar .responsive-nav").removeClass("active-nav");
+        $("#navbar .responsive-nav").slideUp("slow");
         $("#navbar").css("padding-bottom", "30px");
-        
+
+        //Navbar data index
+        var indexNav = $(".right-nav .active").parent().data("index");
+        var indexNavR = $(".responsive-nav .active").parent().data("index");
+        if (width < 768) {
+            $(".responsive-nav li[data-index=" + indexNav + "] a").addClass("active");
+        } else if (width > 768) {
+            $(".right-nav li[data-index=" + indexNavR + "] a").addClass("active");
+        }
+
+        // console.log($(".right-nav .active").parent().data("index"));
+        // console.log(text);
     });
 
 
@@ -18,9 +33,9 @@ $(document).ready(function () {
         $(this).addClass("active");
     })
 
-    //Display&Hide responsive nav
+    //Display&Hide responsive nav    
     $("#navbar .right-nav .menuContainer").click(function () {
-        $("#navbar .responsive-nav").toggleClass("active-nav");
-        $("#navbar").css("padding-bottom","10px");
+        $("#navbar .responsive-nav").slideToggle("slow");
+        $("#navbar").css("padding-bottom", "30px");
     })
 });
