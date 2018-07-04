882d40c6c20ca006703b404d4494a611494fc50f
Shohrat M. Maharramov <shohrat.maharramov@bakubus.local>
2018-07-04 18:00:42 +0400

    Working on navbar: making fixed, language etc

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -1,12 +1,48 @@
 $(document).ready(function () {
 
-    //Functions Executed on window resize
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  << Functions Executed on Window scroll >>  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+    $(window).scroll(function () {
+
+        //Make navber fixed
+        var scrollTopWindow = $(window).scrollTop();
+        if (scrollTopWindow >= 100) {
+            $("#navbar").css({
+                "background-color": "#222",
+                "position": "fixed",
+                "top": "-50px",
+                "left": "0px",
+                "right": "0px"
+            });
+            $("#navbar").animate({
+                top: "0px"
+            }, 300, "linear");
+
+        } else if (scrollTopWindow < 100) {
+            $("#navbar").css({
+                "background-color": "transparent",
+                "position": "absolute",
+                "top": "0px",
+                "left": "0px",
+                "right": "0px",
+            });
+        }
+
+        //Slide Up Lang
+        $("#navbar .row .lang ul").slideUp("slow");
+        shift = 0;
+    })
+    //~~~~~~~~~~~~~~~~~~~~  << End of Window scroll >>  ~~~~~~~~~~~~~~~~~~~~//
+
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  << Functions Executed on Window resize >>  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
     $(window).resize(function () {
 
         //Window width and height
         var width = $(window).width();
         var height = $(window).height();
+
         //Hide responsive nav
         $("#navbar .responsive-nav").slideUp("slow");
         $("#navbar").css("padding-bottom", "30px");
@@ -19,12 +55,69 @@ $(document).ready(function () {
         } else if (width > 768) {
             $(".right-nav li[data-index=" + indexNavR + "] a").addClass("active");
         }
-    });
 
+        //Slide Up Lang
+        $("#navbar .row .lang ul").slideUp("slow");
+        shift = 0;
+    });
+    //~~~~~~~~~~~~~~~~~~~~  << End of Window resize >>  ~~~~~~~~~~~~~~~~~~~~//
+
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  << Functions Executed by Mouse clicked position >>  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    var pageX, pageY;
+    $(document).click(function (event) {
+        pageX = event.pageX;
+        pageY = event.pageY;
+
+        //Slude up Language
+        langPositions($("#navbar .row .lang"), 105, 150);
+        if (!((pageX > elemLeft) && (pageX < elemRight) && (pageY > elemTop) && (pageY < elemBottom))) {
+            $("#navbar .row .lang ul").slideUp("slow");
+            shift = 0;
+        }
+    });
+    //~~~~~~~~~~~~~~~~~~~~  << End of Mouse clicked position >>  ~~~~~~~~~~~~~~~~~~~~//
+
+
+    //~~~~~~~~~~ << New Function : Language position settings >> ~~~~~~~~~~//
+    var elemLeft, elemRight, elemTop, elemBottom, elemPos;
+
+    function langPositions(elem, Width, Height) {
+        if (elem && Width > 0 && Height > 0) {
+            elemPos = elem.offset();
+            elemTop = elemPos.top;
+            elemLeft = elemPos.left;
+            elemRight = (elemLeft + Width);
+            elemBottom = (elemTop + Height);
+        }
+    }
+    //~~~~~~~~~~ << End of Function : Language position settings >> ~~~~~~~~~~//
+
+
+    //Language Slide down
+    var shift = 0;
+    $("#navbar .row .lang p").click(function () {
+        if (shift == 0) {
+            $("#navbar .row .lang ul").slideDown("slow");
+            shift = 1;
+        } else {
+            $("#navbar .row .lang ul").slideUp("slow");
+            shift = 0;
+        }
+    });
+    $("#navbar .row .lang ul li a").click(function (e) {
+        e.preventDefault();
+    });
 
     // Navbar section
     //Adding active class to clicked tab
-    $("#navbar .container ul li a").click(function (e) {
+    $("#navbar .container .right-nav ul li a").click(function (e) {
+        e.preventDefault();
+        $("#navbar .container ul li .active").removeClass("active");
+        $(this).addClass("active");
+    });
+    
+    $("#navbar .container .responsive-nav ul li a").click(function (e) {
         e.preventDefault();
         $("#navbar .container ul li .active").removeClass("active");
         $(this).addClass("active");
@@ -35,9 +128,4 @@ $(document).ready(function () {
         $("#navbar .responsive-nav").slideToggle("slow");
         $("#navbar").css("padding-bottom", "30px");
     })
-
-    //Make navber fixed
-    // var 
-    // console.log();
-
 });
