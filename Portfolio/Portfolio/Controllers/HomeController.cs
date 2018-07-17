5236b09063a1be227b90d9896e6e1e61d73f19ef
Shohrat M. Maharramov <shohrat.maharramov@bakubus.local>
2018-07-17 18:23:01 +0400

    Sending Message complated

--- Portfolio/Portfolio/Controllers/HomeController.cs
+++ Portfolio/Portfolio/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Portfolio.Models;
+using System.Net.Mail;
 
 namespace Portfolio.Controllers
 {
@@ -11,6 +13,26 @@ namespace Portfolio.Controllers
         public ActionResult Index()
         {
             return View();
-        }        
+        }
+
+        
+        [HttpPost]
+        public ActionResult SendMessage(MessagesModel msg)
+        {            
+            MailAddress from = new MailAddress("shohret550@gmail.com", msg.name);
+            MailAddress to = new MailAddress("shohret550@gmail.com", "Shohret");
+
+            MailMessage mailMessage = new MailMessage(from, to);
+
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Subject = msg.subject;
+            mailMessage.Body = msg.text + " Tarix: "+msg.date + "<br/> Email: " + msg.email;          
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Send(mailMessage);
+
+            Session["MessageSent"] = true;
+            return RedirectToAction("index");
+        }
     }
 }
---
+++ Portfolio/Portfolio/Models/MessagesModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    public class MessagesModel
+    {        
+        [Display(Name = "Name")]
+        [Required]
+        public string name { get; set; }
+
+        [DataType(DataType.EmailAddress), Display(Name = "Email")]
+        [Required]
+        public string email { get; set; }
+
+        [Display(Name = "Subject")]
+        public string subject { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Text")]
+        public string text { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date")]
+        public string date { get; set; }
+    }
+}
